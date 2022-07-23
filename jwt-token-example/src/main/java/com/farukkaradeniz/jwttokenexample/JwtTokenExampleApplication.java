4fcf5db6a2c6ecafcd9b7279c0f04d7e8e2728b1
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-07-24 00:51:24 +0300

    create jwt-token-example app

---
+++ jwt-token-example/src/main/java/com/farukkaradeniz/jwttokenexample/JwtTokenExampleApplication.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.jwttokenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTokenExampleApplication.class, args);
+    }
+
+}
