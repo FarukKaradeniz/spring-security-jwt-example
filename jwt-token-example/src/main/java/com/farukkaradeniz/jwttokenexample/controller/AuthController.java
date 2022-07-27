b5fabb34c147824b87e8703576c3f0d4ecef4478
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-07-27 14:22:25 +0300

    password check

--- jwt-token-example/src/main/java/com/farukkaradeniz/jwttokenexample/controller/AuthController.java
+++ jwt-token-example/src/main/java/com/farukkaradeniz/jwttokenexample/controller/AuthController.java
@@ -3,10 +3,13 @@ package com.farukkaradeniz.jwttokenexample.controller;
 import com.farukkaradeniz.jwttokenexample.dto.LoginModel;
 import com.farukkaradeniz.jwttokenexample.dto.TokenModel;
 import com.farukkaradeniz.jwttokenexample.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AuthController {
@@ -18,7 +21,11 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenModel> login(@RequestBody LoginModel loginModel) {
-        var token = authService.createToken(loginModel.username());
+        var token = authService.createToken(loginModel.username(), loginModel.password());
+
+        if (!StringUtils.hasText(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "invalid password");
+        }
 
         return ResponseEntity.ok(new TokenModel(token));
     }
--- jwt-token-example/src/main/java/com/farukkaradeniz/jwttokenexample/service/AuthService.java
+++ jwt-token-example/src/main/java/com/farukkaradeniz/jwttokenexample/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.farukkaradeniz.jwttokenexample.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.time.Instant;
 
@@ -12,16 +14,24 @@ public class AuthService {
     private final JwtEncoder jwtEncoder;
     private final UserService userService;
 
-    public AuthService(JwtEncoder jwtEncoder, UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(JwtEncoder jwtEncoder, UserService userService, PasswordEncoder passwordEncoder) {
         this.jwtEncoder = jwtEncoder;
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public String createToken(String username) {
+    public String createToken(String username, String password) {
         Instant now = Instant.now();
         long expiry = 36000L;
 
         var user = userService.getUser(username);
+
+        Assert.notNull(password, "Password should not be null");
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            return "";
+        }
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
